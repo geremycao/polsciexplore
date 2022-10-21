4095d8518a32d9c468e10b61fac6e12bfb781916
joshuajohnmills <josh97mills@gmail.com>
2022-10-21 15:37:41 +1100

    getup html

--- plt_script.js
+++
@@ -1,8 +0,0 @@
-
-
-//Drawing a line
-var c = document.getElementById("myCanvas");
-            var ctx = c.getContext("2d");
-            ctx.moveTo(0, 0);
-            ctx.lineTo(200, 100);
-            ctx.stroke();
